13e031b765456143dc80eeaead9bdb7671e82003
ritik2727 <ritikjain2727@gmail.com>
2023-11-04 21:18:34 +0530

    first commit

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,120 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+const styles = {
+  popup: {
+    // position: "fixed",
+    // top: "50%",
+    // left: "50%",
+    // transform: "translate(-50%, -50%)",
+    width: "500px",
+    height: "500px",
+    border: "1px solid black",
+    overflow: "hidden",
+    backgroundColor: "white",
+    zIndex: "9999"
+  },
+  popupHeader: {
+    backgroundColor: "#f1f1f1",
+    padding: "10px",
+    textAlign: "right"
+  },
+  closeIcon: {
+    fontSize: "20px",
+    cursor: "pointer",
+    color: "black"
+  },
+  popupContent: {
+    padding: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%"
+  },
+  responseContainer: {
+    textAlign: "center",
+    marginBottom: "20px",
+    color: "black"
+  },
+  inputContainer: {
+    display: "flex",
+    alignItems: "center"
+  },
+  commandInput: {
+    flex: "1",
+    padding: "5px",
+    marginRight: "10px",
+    backgroundColor: "white",
+    borderRadius: 20,
+    color: "black"
+  },
+  generateButton: {
+    padding: "5px 10px",
+    cursor: "pointer",
+    color: "white",
+    backgroundColor: "green"
+  }
+} as { [key: string]: React.CSSProperties }
 
+function IndexPopup({ onClose }) {
+  const [command, setCommand] = useState<string>("")
+  const [response, setResponse] = useState<string>("")
+
+  const handleGenerate = async () => {
+    try {
+      const apiResponse = await fetch(
+        `http://localhost:3000/api/generate?command=${encodeURIComponent(
+          command
+        )}`
+      )
+
+      if (!apiResponse.ok) {
+        throw new Error(`API request failed with status: ${apiResponse.status}`)
+      }
+
+      const responseData = await apiResponse.json()
+
+      // Parse the streamingResponse JSON string
+      const streamingResponseArray = JSON.parse(responseData.streamingResponse)
+
+      console.log(streamingResponseArray)
+
+      // Access the generated_text property
+      const generatedText =
+        streamingResponseArray[0]?.generated_text ||
+        "No generated text available"
+
+      setResponse(generatedText)
+
+      // setResponse(`sucees : ${data.response}`);
+    } catch (error) {
+      console.error("Error during API request:", error)
+      setResponse(`Error during API request ${error}`)
+    }
+  }
+
+  useEffect(() => {}, [response, command, setResponse])
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div style={styles.popup}>
+      <div style={styles.popupHeader}>
+        <span style={styles.closeIcon} onClick={onClose}>&times;</span>
+      </div>
+      <div style={styles.popupContent}>
+        <div style={styles.responseContainer}>{response}</div>
+        <div style={styles.inputContainer}>
+          <input
+            type="text"
+            id="commandInput"
+            placeholder="Enter your command"
+            style={styles.commandInput}
+            value={command}
+            onChange={(e) => setCommand(e.target.value)}
+          />
+          <button style={styles.generateButton} onClick={handleGenerate}>
+            Generate
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
